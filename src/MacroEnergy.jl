fc70d702f1cbd121506c62e8b5d764c00241375b
Emil Dimanchev <emild@mit.edu>
2025-06-13 15:09:35 -0400

    verification test

--- src/MacroEnergy.jl
+++ src/MacroEnergy.jl
@@ -131,7 +131,6 @@ include("model/optimizer.jl")
 include("model/scaling.jl")
 include("model/solver.jl")
 include("model/myopic.jl")
-include("model/learning.jl")
 include_all_in_folder("model/constraints")
 include_all_in_folder("model/benders")
 
@@ -216,7 +215,6 @@ export AbstractAsset,
     MaxCapacityConstraint,
     MaxCapacityGrowthConstraint,
     DevelopmentConstraint,
-    get_tech_ids,
     MaxNewCapacityConstraint,
     MaxNonServedDemandConstraint,
     MaxNonServedDemandPerSegmentConstraint,
