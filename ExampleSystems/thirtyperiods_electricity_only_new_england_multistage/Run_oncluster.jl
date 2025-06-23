26125b09b23ce6fd40d38b9ac500863c9332755b
Emil Dimanchev <emild@mit.edu>
2025-06-23 11:35:18 -0400

    test

--- ExampleSystems/thirtyperiods_electricity_only_new_england_multistage/Run_oncluster.jl
+++ ExampleSystems/thirtyperiods_electricity_only_new_england_multistage/Run_oncluster.jl
@@ -1,8 +1,9 @@
 case = dirname(@__FILE__);
 
 import Pkg
+Pkg.instantiate()
 println("Activating Package")
 
-Pkg.activate("/home/ed0400/Macro")
+Pkg.activate("/home/ed0400/Macro/MacroEnergy")
 
-include("/home/ed0400/Macro/ExampleSystems/thirtyperiods_electricity_only_new_england_multistage/run.jl");
+include("/home/ed0400/Macro/MacroEnergy/ExampleSystems/thirtyperiods_electricity_only_new_england_multistage/run.jl");
--- ExampleSystems/thirtyperiods_electricity_only_new_england_multistage/jobscript.sh
+++ ExampleSystems/thirtyperiods_electricity_only_new_england_multistage/jobscript.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-#SBATCH --job-name=ed_test        # create a short name for your job
+#SBATCH --job-name=ed_test2        # create a short name for your job
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=21       # cpu-cores per task (>1 if multi-threade>
-#SBATCH --mem-per-cpu=1GB       # memory per cpu-core 
-#SBATCH --time=1:00:00          # total run time limit (HH:MM:SS)
+#SBATCH --cpus-per-task=32       # cpu-cores per task (>1 if multi-threade>
+#SBATCH --mem=160GB              # memory per cpu-core 
+#SBATCH --time=2:00:00          # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=all          # send email when job ends
 #SBATCH --mail-user=ed0400@princeton.edu
-######## #SBATCH --exclude=della-h12n16
-######## #SBATCH --constraint=cascade
-######## #SBATCH --nodelist=della-h12n16
 
 module purge
-module load gurobi
-module load julia/1.9.1
+module load gurobi/12.0.0
+module load julia/1.11.1
 
 julia Run_oncluster.jl
